2eeabb1ac156ad1b071e6070408d01a8df443c8f
Palvi Singla <palvisingla017@gmail.com>
2024-05-08 19:36:22 +0530

    Adding LRU Cache Code

---
+++ LRUCache/src/main/java/lrucache/LRUCache.java
@@ -0,0 +1,19 @@
+public class LRUCache {
+    public static void main(String[] args)  {
+        lrucache.CacheOperations cache = new lrucache.CacheOperations(3);
+        cache.add(1, "One");
+        cache.add(2, "Two");
+        cache.add(3, "Three");
+
+
+        System.out.println(cache.get(1));
+
+        cache.add(4, "Four");
+        cache.add(1, "Four");
+
+        System.out.println(cache.get(2));
+
+    }
+
+
+}
